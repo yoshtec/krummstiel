4e83835bab1980a3e052b688057f985a6c54f807
yoshtec <yoshtec@gmx.net>
2021-01-04 23:09:56 +0100

    added trace class and mechanisms

--- dickstiel.py
+++ dickstiel.py
@@ -10,18 +10,36 @@
 
 import shlex
 import sys
-import logging
 import shutil
 from pathlib import Path
 
 ENC = "utf-8"
 
 
-def check_call(args, ignore_return_code=False):
+class Trace:
+    def __init__(self, debug=None, info=print, error=print):
+        self._debug: callable = debug
+        self._info: callable = info
+        self._error: callable = error
+
+    def error(self, msg):
+        if self._error:
+            self._error(msg)
+
+    def info(self, msg):
+        if self._info:
+            self._info(msg)
+
+    def debug(self, msg):
+        if self._debug:
+            self._debug(msg)
+
+
+def check_call(args, ignore_return_code=False, trace=Trace()):
     import subprocess
 
     cmd_str = " ".join(args)
-    print(f"Execute command: '{cmd_str}'")
+    trace.debug(f"Execute command: '{cmd_str}'")
     p = subprocess.Popen(
         args,
         stdout=subprocess.PIPE,
@@ -31,18 +49,20 @@ def check_call(args, ignore_return_code=False):
     )
     stdout, stderr = p.communicate()
     if stdout:
-        # logging.debug(stdout)
-        print(f"stdout: {stdout}")
+        trace.debug(f"stdout: \n{stdout}")
     if stderr:
-        # logging.debug(stderr)
-        print(f"stderr: {stderr}")
+        trace.debug(f"stderr: \n{stderr}")
     if not ignore_return_code and p.returncode != 0:
         raise RuntimeError(f"failed to run '{cmd_str}'")
     return stdout
 
 
 class MiDevice:
-    def __init__(self, uid: str, base_path: Path, alias: str, exclude=None):
+    def __init__(self, uid: str, base_path: Path, alias: str, exclude=None, trace=Trace()):
+
+        if not trace:
+            trace = Trace()
+        self.trace: Trace = trace
 
         self.uid: str = uid
         self.alias: str = alias
@@ -61,7 +81,7 @@ class MiDevice:
             devices = check_call(["idevice_id", "-l"])
             return self.uid in devices
         except RuntimeError as e:
-            print(f"error while checking for devices {e}")
+            self.trace.error(f"error while checking for devices {e}")
         return False
 
     def mount(self):
@@ -74,7 +94,7 @@ class MiDevice:
             check_call(cmd)
             self.is_mounted = True
         except RuntimeError as e:
-            print(f"Error while mounting: {e}")
+            self.trace.error(f"Error while mounting: {e}")
 
     def umount(self):
         if not self.is_mounted:
@@ -85,14 +105,14 @@ class MiDevice:
             check_call(cmd)
             self.is_mounted = False
         except RuntimeError as e:
-            print(f"Error while unmounting {self._mount_point}, with {e}")
-            print("Do not unplug the iOS device while Backup is running")
-            print("try running:")
-            print(f"    sudo umount --force {self._mount_point}")
+            self.trace.error(f"Error while unmounting {self._mount_point}, with {e}")
+            self.trace.error("Do not unplug the iOS device while Backup is running")
+            self.trace.error("try running:")
+            self.trace.error(f"    sudo umount --force {self._mount_point}")
 
     def backup(self):
         if not self.is_mounted:
-            print(f"cannot backup {self.alias}: device not mounted")
+            self.trace.debug(f"cannot backup {self.alias}: device not mounted")
             return
 
         try:
@@ -104,17 +124,17 @@ class MiDevice:
 
             cmd = ["rsync", "-avzh"]
             for e in self.exclude:
-                cmd.extend(["--exclude", e])
+                cmd.extend(["--exclude", shlex.quote(e)])
             cmd.extend([str(self._mount_point), str(self.target)])
             check_call(cmd)
 
         except RuntimeError as e:
-            print(f"error while backup of {self.alias}: {e}")
-            logging.error(e)
+            self.trace.error(f"error while backup of {self.alias}: {e}")
+            # logging.error(e)
 
     def prune_photos(self):
         if not self.is_mounted:
-            print(f"cannot prune {self.alias}: device not mounted")
+            self.trace.debug(f"cannot prune {self.alias}: device not mounted")
             return
 
         # cleanup of iphone: delete photos and videos of certain age
@@ -135,16 +155,16 @@ class MiDevice:
             devices = check_call(["idevicepair", "list"])
             return self.uid in devices
         except RuntimeError as e:
-            print(f"Error while checking if {self.uid} is paired with this host: {e}")
+            self.trace.error(f"Error while checking if {self.uid} is paired with this host: {e}")
         return False
 
     def pair(self):
         try:
             # have to wait for user interaction.
-            cmd = ["idevicepair", "-u", self.uid, "pair"]
+            cmd = ["idevicepair", "-u", shlex.quote(self.uid), "pair"]
             check_call(cmd)
         except RuntimeError as e:
-            print(e)
+            self.trace.error(e)
 
     @classmethod
     def discover(cls):
@@ -200,17 +220,21 @@ def main(argv):
         sys.stdout.write(__doc__)
         return 0
 
+    trace = Trace()
+    if pa.verbose:
+        trace = Trace(debug=print, error=print, info=print)
+
     # Check for prereq
     required_commands = ["idevicepair", "ifuse", "rsync", "umount", "idevice_id"]
     for cmd in required_commands:
         satisfied = True
         if shutil.which(cmd) is None:
-            print(f"cmd: {cmd} is missing")
+            trace.error(f"cmd: {cmd} is missing")
             satisfied = False
 
         if not satisfied:
-            print(f"Requirements are missing! please install")
-            print(f"    apt install libimobiledevice ")
+            trace.error(f"Requirements are missing! please install")
+            trace.error(f"    apt install libimobiledevice ")
             return 1
 
     if pa.config:
@@ -236,16 +260,16 @@ def main(argv):
 
             name = config.get(s, "name")
             if name is None:
-                print(f"name is missing from config section: {s}")
+                trace.error(f"name is missing from config section: {s}")
                 continue
 
             if config.getboolean(s, "ignore", fallback=False):
-                print(f"ignoring device {name} with uid: {s}")
+                trace.info(f"ignoring device {name} with uid: {s}")
                 continue
 
             backup_path = config.get(s, "backup_path")
             if backup_path is None:
-                print(f"backup_path is missing from config section {s} or in [DEFAULT] section")
+                trace.error(f"backup_path is missing from config section {s} or in [DEFAULT] section")
                 continue
             backup_path = Path(backup_path)
 
@@ -257,24 +281,24 @@ def main(argv):
                     try:
                         exclude = json.loads(exclude)
                     except RuntimeError as e:
-                        print(
+                        trace.error(
                             f"error reading exclude array from config section {s}, error={e}"
                         )
-                        print(f"stopping backup for device {name} with uid {s} ")
+                        trace.error(f"stopping backup for device {name} with uid {s} ")
                         continue
                 else:
                     # only single to exclude
                     exclude = [exclude]
 
-            device = MiDevice(uid=s, base_path=backup_path, alias=name, exclude=exclude)
+            device = MiDevice(uid=s, base_path=backup_path, alias=name, exclude=exclude, trace=trace)
 
             if not device.is_present:
-                print(f"device {name} with uid {s} not here, skipping")
+                trace.info(f"device {name} with uid {s} not connected. skipping!")
                 continue
 
             if not device.check_paired():
-                print(f"please pair your device {device.alias} uid={device.uid} by executing:")
-                print(f"    idevicepair -u {device.uid}")
+                trace.info(f"please pair your device {device.alias} uid={device.uid} by executing:")
+                trace.info(f"    idevicepair -u {device.uid}")
                 continue
 
             device.mount()
